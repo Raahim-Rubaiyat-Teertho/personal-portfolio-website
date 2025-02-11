32a8e7418bca9a14f46229b9ab7405c2817c9866
Teertho <raahim.rubaiyat2021@gmail.com>
2025-02-11 22:02:07 +0600

    changes 1

---
+++ portfolio-website/src/components/about.jsx
@@ -0,0 +1,49 @@
+import { motion, useMotionValue, useTransform, animate } from "framer-motion";
+
+export default function AboutMe() {
+  return (
+    <div
+      className="h-screen w-full flex flex-col justify-center items-center"
+      id="about"
+    >
+      <div className="md:grid grid-cols-2 gap-2">
+        <div className="flex flex-col justify-center items-center">
+          {/* <h1 className="text-6xl font-space pb-10 text-center">About Me</h1> */}
+          <motion.p
+            className="text-center font-space px-6 md:px-24"
+            initial={{ opacity: 0, y: -50 }} // Start above and invisible
+            whileInView={{ opacity: 1, y: 0 }} // Animate when in view
+            transition={{ duration: 1, delay: 0 }} // Smooth animation
+            viewport={{ once: true, amount: 0.3 }} // Starts after typing finishes
+          >
+            <h1 className="text-6xl font-space pb-10">About Me</h1>
+            <p className="text-gray-200 pb-10">
+              Hi, I'm a 24-year-old computer science student from Dhaka,
+              Bangladesh, studying at BRAC University. I'm passionate about
+              building software and exploring new tech like AI and machine
+              learning. I enjoy learning every day and trying my hand at
+              projects that simplify life in small ways. When I'm not coding,
+              you'll likely find me making music—a creative way to relax and
+              express myself.
+            </p>
+          </motion.p>
+        </div>
+        <div className="flex justify-center items-center">
+          <motion.p
+            className="text-center font-space px-24"
+            initial={{ opacity: 0, y: 50 }} // Start above and invisible
+            whileInView={{ opacity: 1, y: 0 }} // Animate when in view
+            transition={{ duration: 0.5, delay: 0 }} // Smooth animation
+            viewport={{ once: true, amount: 0.4 }} // Starts after typing finishes
+          >
+            <img
+              className="rounded-full md:size-72 object-cover"
+              src="./273578610_5162929663771084_7411328744564490725_n(1).jpg"
+              alt="my-picture"
+            />
+          </motion.p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ portfolio-website/src/components/intro.jsx
@@ -0,0 +1,54 @@
+import { motion, useMotionValue, useTransform, animate } from "framer-motion";
+import { TypewriterEffectSmooth } from "./ui/typewriter-effect";
+import { useEffect } from "react";
+
+export default function Intro() {
+  const words = [
+    {
+      text: "Hi, ",
+    },
+    {
+      text: "I'm ",
+    },
+    {
+      text: "Teertho",
+    },
+  ];
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center">
+      {/* <motion.span className="text-6xl font-space pb-10">
+        {displayText}
+      </motion.span> */}
+      <TypewriterEffectSmooth
+        words={words}
+        speed={10}
+        // deleteSpeed={20}
+        // wrapperClassName="text-center font-space  text-gray-200"
+        className={"text-6xl :text-6xl text-center"}
+      />
+      <motion.p
+        className=" text-center font-space sm:px-52 md:px-72 px-7 text-sm"
+        initial={{ opacity: 0, y: -50 }} // Start above and invisible
+        animate={{ opacity: 1, y: 0 }} // Slide down & fade in
+        transition={{ duration: 0.7, delay: 0.7 }} // Starts after typing finishes
+      >
+        I'm a software engineer from Dhaka, Bangladesh who loves building
+        software, exploring AI/ML, and learning something new every day. Music
+        is my creative escape.
+      </motion.p>
+
+      <motion.p
+        className=" text-center font-space sm:px-52 md:px-72 px-7 text-sm"
+        initial={{ opacity: 0, y: -50 }} // Start above and invisible
+        animate={{ opacity: 1, y: 0 }} // Slide down & fade in
+        transition={{ duration: 0.7, delay: 0.7 }} // Starts after typing finishes
+      >
+        <a href="#about">
+          <button className="mt-7 px-3 py-2 border-white border-2 bg-white text-slate-950 rounded-lg font-medium">
+            More About Me
+          </button>
+        </a>
+      </motion.p>
+    </div>
+  );
+}
